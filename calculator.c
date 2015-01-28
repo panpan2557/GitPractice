377e95b3ae1027a5db73ce5e0d35aad77724b1ff
panpan2557 <kikuboy_123@hotmail.com>
2015-01-28 18:21:42 +0700

    clear

--- calculator.c
+++ calculator.c
@@ -1,55 +1,6 @@
 #include <stdio.h>
 
-int mul( int x, int y ){
-    return x*y;
-}
-
-int div(int x, int y) {
-	return x/y;
-}
-
-int add(int x ,int y){
-	return x + y;
-}
-
-int subtract(int num1,  num2) {
-	return num1-num2;
-}
-
-int findMax() {
-	printf("Searching for Max . . .\n");
-	printf("Max(mi) found at room 201 with iNont\n");
-	return 201;
-}
-
-int avg( int a, int b, int c ){
-    return (a+b+c)/3;
-}
-
 int main() {
 	printf("calculator");
-
-	printf("This test is run by Momo : \n9 - 2 = %d\n", subtract(9,2) );
-
-    	printf(mul(1,2));
-
-    	printf(add(2,1));
-
-	printf("div : 5 / 5 = %d\n", div(5,5));
-
-	printf("\n\n Momo want to findMax, so he call findMax() method\n");
-	printf("findMax() = %d\n", findMax());
-	
-
-	printf("test findSumBranchhhhhhhhhh");
-	printf("-\n-\n-\n-\n-\n-\n-\n-");
-
-
-    printf("average of 1,2,3 = %d\n", avg( 1,2,3 ));
-    printf("momo");
-    printf("why you");
-    printf("are so");
-    printf("HANDSOME!!!");
-
 }
 
