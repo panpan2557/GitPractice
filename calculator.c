bbb9253ed73fddd5bb7c476bcc3c72a07074025c
Tanatorn Assawaamnuey <neen_tta@hotmail.com>
2015-01-28 17:39:57 +0700

    naneen create mul method

--- calculator.c
+++ calculator.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+int mul( int x, int y ){
+    return x*y;
+}
+
 int main() {
 	printf("calculator");
 	printf("Martin");
     printf("naneen is so cute");
 	printf("PP narak jung");
-
 }
