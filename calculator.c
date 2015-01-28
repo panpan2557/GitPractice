0d00a195e5c3b3c53f8cd60b9c89a2db5c542011
Momomomomo <topolite@hotmail.com>
2015-01-28 17:38:20 +0700

    momo update

--- calculator.c
+++ calculator.c
@@ -2,19 +2,8 @@
 
 int main() {
 	printf("calculator");
-<<<<<<< HEAD
-<<<<<<< HEAD
 	printf("The legendary of Momomomomomo");
-=======
-    printf("naneen is so cute");
+    	printf("naneenyjung is so cute");
 	printf("PP narak jung");
->>>>>>> 24b55051a4bc8378826b58e582cf62300be598bf
-
-	
-=======
 	printf("Martin");
-    printf("naneen is so cute");
-	printf("PP narak jung");
-
->>>>>>> e0466e78ce03eefa9ecc937ea0c32bebb007a964
 }
