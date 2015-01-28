379fc5a62b9ba75d54788f763d59f1b933207e81
panpan2557 <kikuboy_123@hotmail.com>
2015-01-28 18:27:41 +0700

    add more comment

--- calculator.c
+++ calculator.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
-//calculator
+//add(x,y)
+
+
+//sub(x,y)
+
+
+//mul(x,y)
+
+
+//div(x,y)
+
+
+//pow(x,y)
+
+
+//sqrt(x,y)
+
+
+//findmin(x,y)
+
+
+//findmax(x,y)
+
+
+//findavg(x,y)
+
+
 int main() {
 	printf("calculator");
 }
-
