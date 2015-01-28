40794ba96febb033b14b2e599dfd23d8328cf640
Momomomomo <topolite@hotmail.com>
2015-01-28 17:55:28 +0700

    pp

--- calculator.c
+++ calculator.c
@@ -18,20 +18,13 @@ int subtract(int num1,  num2) {
 
 int main() {
 	printf("calculator");
-	printf("The legendary of Momomomomomo");
-    	printf("naneenyjung is so cute");
-	printf("PP narak jung");
-	printf("Martin");
-	printf("My name is ChaoChao");
 
-	printf("This test is run Momo : \n9 - 2 = %d\n", subtract(9,2) );
+	printf("This test is run by Momo : \n9 - 2 = %d\n", subtract(9,2) );
+
     	printf(mul(1,2));
 
-	printf("calculator");    
-    	printf(mul(1,2));
 	printf("div : 5 / 5 = %d\n", div(5,5));
-    
-    	printf("martin said KUY");
+
 
 }
 
